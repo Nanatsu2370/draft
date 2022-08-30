8c8cc46253f87fdb13a80cf2441212da8c4066a7
Nanatsu2370 <94955661+Nanatsu2370@users.noreply.github.com>
2022-08-30 11:21:20 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
 api_hash = "e3ce8145aa657c2a4cc5cf0f7183e476"
 api_id = 13233271
-bot_token = "5468195080:AAGcb6RCNJParhJ2Vt-OtFp3PG5nETQMCJo"
-bot_admin_group = -1001505671370
+bot_token = "5522127227:AAHWgU2hJFS-4htcpIxHfi6LhSQQpBg85n0"
+bot_admin_group = -631263640
