09b3a05fd5918218b48e185ce97eb2ccf6f5b139
Nanatsu2370 <94955661+Nanatsu2370@users.noreply.github.com>
2022-09-01 14:25:46 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,4 +1,4 @@
 api_hash = "e3ce8145aa657c2a4cc5cf0f7183e476"
 api_id = 13233271
-bot_token = "5522127227:AAHWgU2hJFS-4htcpIxHfi6LhSQQpBg85n0"
+bot_token = "5522127227:AAGqlmlfgew8xtTnUzlN_V184nfWEqGBYH8"
 bot_admin_group = -631263640
